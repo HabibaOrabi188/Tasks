210ee98c924fe19b7116999040da82d8c5ca20bb
HabibaOrabi188 <habibaorabi57@gmail.com>
2024-01-16 22:49:18 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -11,7 +11,6 @@ import { styled } from 'nativewind';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
 import Block1 from './Pages/Block1';
 import Block2 from './Pages/Block2';
 import Block3 from './Pages/Block3';
@@ -20,7 +19,6 @@ import Block5 from './Pages/Block5';
 import Block6 from './Pages/Block6';
 import Block7 from './Pages/Block7'
 import Block8 from './Pages/Block8';
-import Block9 from './Pages/Block9';
 import Block10 from './Pages/Block10';
 const StyledView = styled(View)
 
@@ -28,7 +26,7 @@ const StyledText = styled(Text)
 
 const App = () => {
   return (
-    <Block9/>
+    <Block10/>
   );
 
 }
